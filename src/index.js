177e9afb906e5d3717a56bc0b99b87898c306b25
nodejs <shinchou@shinchou.local>
2016-10-07 17:38:02 +0800

    route

--- src/index.js
+++ src/index.js
@@ -4,28 +4,32 @@ import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import Counter from './Components/Counter'
 import reducer from './Reducer'
+import routes from '../routes'
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 import NotFound from './Components/404.js'
 import Hello from './Components'
+
 // Store
 const store = createStore(
   combineReducers(Object.assign(reducer, {
   routing: routerReducer
   })
-
-
-
   )
 )
-const history = syncHistoryWithStore(browserHistory, store)
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={Counter}></Route>
-      <Route path="hello" component={Hello}/>
-      <Route path="*" component={NotFound}/>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-)
+// const history = syncHistoryWithStore(browserHistory, store)
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <Route path="/" component={Counter}></Route>
+//       <Route path="hello" component={Hello}/>
+//       <Route path="*" component={NotFound}/>
+//     </Router>
+//   </Provider>,
+//   document.getElementById('root')
+  ReactDOM.render(((history) => {
+  return <Provider store={store}>
+    <Router routes={routes} history={history} />
+  </Provider>
+})(syncHistoryWithStore(browserHistory, store)), document.getElementById('root'))
+
